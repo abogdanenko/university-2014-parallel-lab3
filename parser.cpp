e426ca1a95ced10278016753ba7cd52a2d602e32
Alexey Bogdanenko <alexey@bogdanenko.com>
2014-11-30 20:14:10 +0300

    parser: Rename executable

--- parser.cpp
+++ parser.cpp
@@ -26,7 +26,7 @@ Parser::Parser(const int argc, char** const argv):
 
 void Parser::PrintUsage()
 {
-    cout << "Usage: lab3-sequential "
+    cout << "Usage: lab3 "
             "[-h] "
             "[-v] "
             "[-n size] "
